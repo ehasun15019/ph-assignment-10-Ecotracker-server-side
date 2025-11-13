057bac8b1eac4d7096f7a225cad2aae4a0d0a37a
ehasun15019 <ehasunulislam0202@gmail.com>
2025-11-13 21:32:53 +0600

    upload in vercel

--- index.js
+++ index.js
@@ -362,7 +362,7 @@ async function run() {
     /*  admin created challenges api end  */
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
